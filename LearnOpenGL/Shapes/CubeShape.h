db3d8432a5ce34f76859e374e173d9dc8a5c8509
vadimsZinatulins <vadimz2@hotmail.com>
2020-09-04 00:15:06 +0100

    Added missing files -.-

---
+++ LearnOpenGL/Shapes/CubeShape.h
@@ -0,0 +1,14 @@
+#ifndef __CUBE_SHAPE_H__
+#define __CUBE_SHAPE_H__
+
+#include <vector>
+
+namespace Shapes
+{
+
+extern std::vector<float> cubeVertices;
+extern std::vector<float> cubeUvs;
+
+}
+
+#endif // !__CUBE_SHAPE_H__
